2e793c8175e18cb3a153aab13fd38ba34aaf46c7
Elton Gonçalves Gomes <eltongonc@gmail.com>
2021-03-28 03:28:11 +0200

    repo commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cd0684ba910abba8c0253f42d1c813bf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4ca8dafb-a934-4c93-8b6b-6a4fa60652f1'
+};
